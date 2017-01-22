2adeb02f2f608b719a0449597811682a8a1477e3
wing <ioridx777@yahoo.com.hk>
2017-01-22 13:45:03 +0800

    update anim

--- Assets/Resources/Characters/C1(Back)/SpineTest.cs
+++ Assets/Resources/Characters/C1(Back)/SpineTest.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-using UnityEngine;
-=======
+
 ﻿using UnityEngine;
->>>>>>> 36271bfabb5c44cc67a1733577195129b4fa9bf7
 using System.Collections;
 using Spine.Unity;
 
@@ -17,19 +14,16 @@ public class SpineTest : MonoBehaviour {
 	[SpineAnimation]
 	public string attack;
 
-<<<<<<< HEAD
     public enum AnimationState { 
         Attack01=0,Attack02=1,Hit=2,Idle_Fast=3,Idle_Slow=4,Jump=5
     }
 
-=======
->>>>>>> 36271bfabb5c44cc67a1733577195129b4fa9bf7
 	void Start () {
 		skeletonAnimation = GetComponent<SkeletonAnimation> ();
 		skeletonAnimation.loop = true;
 		skeletonAnimation.AnimationName = idle;
 		skeletonAnimation.state.Complete += delegate {
-			if (skeletonAnimation.AnimationName== attack) {
+			if (!checkAnimationAvilable()) {
 				skeletonAnimation.loop = true;
 				skeletonAnimation.AnimationName = idle;
 			}
@@ -38,36 +32,32 @@ public class SpineTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
         
         if (Input.GetKeyUp(KeyCode.A))
         {
             //skeletonAnimation.loop =true;
             //skeletonAnimation.AnimationName = AnimationState.Jump.ToString();
             //PlayerNetworkManager.action = "jump";;
-            
-            skeletonAnimation.state.AddAnimation(1, AnimationState.Jump.ToString(), false, 0);
-            skeletonAnimation.state.AddAnimation(0, AnimationState.Idle_Fast.ToString(), true, 
-            skeletonAnimation.skeletonDataAsset.duration[(int)AnimationState.Jump]);
+            Jump();
 		}
 
 	}
 
     public void Jump()
     {
-        skeletonAnimation.loop = false;
-        skeletonAnimation.AnimationName = AnimationState.Jump.ToString();
-        
-        PlayerNetworkManager.action = "jump";
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Jump.ToString(), false, 0);
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Idle_Slow.ToString(), true, 0);
     }
     public void Attack()
     {
-        skeletonAnimation.loop = true;
-        skeletonAnimation.AnimationName = attack;
-        PlayerNetworkManager.action = "attack";
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Attack01.ToString(), false, 0);
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Idle_Slow.ToString(), true, 0);
     }
     public void Injured()
-    { 
+    {
+
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Hit.ToString(), false, 0);
+        skeletonAnimation.state.AddAnimation(0, AnimationState.Idle_Slow.ToString(), true, 0);
     }
     public bool checkAnimationAvilable()
     {
@@ -77,15 +67,7 @@ public class SpineTest : MonoBehaviour {
     public bool checkIsDodging()
     {
         return skeletonAnimation.AnimationName == "Jump";
-    }
-=======
-		if (Input.GetKeyUp(KeyCode.A)) {
-			skeletonAnimation.loop = true;
-			skeletonAnimation.AnimationName = attack;
-			PlayerNetworkManager.action = "attack";
-		} 
 	}
 
 		
->>>>>>> 36271bfabb5c44cc67a1733577195129b4fa9bf7
 }
